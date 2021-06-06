59a70d8640717012d071b4cf50b6d6f84a5b295c
Ararat1 <matinyan_0@mail.ru>
2021-06-06 20:27:21 +0400

    Updated TeahcerModel

--- models/TeacherModel.js
+++ models/TeacherModel.js
@@ -11,7 +11,7 @@ const TeacherSchema = new Schema({
   },
   patronymic: {
     type: String,
-    default: ""
+    default: "֊"
   },
   institution: { type: Schema.Types.ObjectId, ref: "institutions" },
   subjects: [{ type: Schema.Types.ObjectId, ref: "subjects" }]
