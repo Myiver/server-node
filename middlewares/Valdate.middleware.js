4c25044146c37910c5b61ddcc833f73988cd9198
Ararat1 <matinyan_0@mail.ru>
2021-06-07 11:07:29 +0400

    Added a new teacher fields validation

--- middlewares/Valdate.middleware.js
+++ middlewares/Valdate.middleware.js
@@ -39,6 +39,27 @@ class Validate {
       return res.json({ error: message })
     }
   }
+
+  static async newTeacher(req, res, next) {
+    try {
+      const schema = Joi.object({
+        firstName: Joi.string().min(2).max(64).required(),
+        lastName: Joi.string().min(2).max(64).required(),
+        patronymic: Joi.string().max(64),
+        subjects: Joi.array().min(1).required()
+      })
+
+      const { error } = schema.validate(req.body)
+
+      if (error) {
+        return res.json({ error: error.details[0].message })
+      }
+
+      return next()
+    } catch ({ message }) {
+      return res.json({ error: message })
+    }
+  }
 }
 
 export { Validate }
--- routers/Teacher.js
+++ routers/Teacher.js
@@ -1,11 +1,12 @@
 import express from 'express'
 
 import { TeacherController } from "../controllers"
+import { Validate } from "../middlewares"
 
 const router = express.Router()
 
 /* POST create a new teacher*/
-router.post("/new", TeacherController.create)
+router.post("/new", Validate.newTeacher, TeacherController.create)
 
 /* POST get teachers list by institution _id */
 router.post("/get-list", TeacherController.getList)
